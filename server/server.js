4ee71d3a7fe2db2006650417c2c6b4fb88b6f019
NguyenHuuHaiHoang1810 <95134351+NguyenHuuHaiHoang1810@users.noreply.github.com>
2022-03-17 23:55:30 +0700

    Fifth Commit

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const URI = process.env.MONGODB_URL;
 mongoose.connect(
   URI,
   {
+    useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
