a18bb8d9cfa769173daacfc6c091c7bd7633bee6
NguyenHuuHaiHoang1810 <nguyenhuuhaihoang1810@gmail.com>
2022-03-18 21:00:16 +0700

    Sync

--- server/server.js
+++ server/server.js
@@ -13,14 +13,12 @@ const URI = process.env.MONGODB_URL;
 mongoose.connect(
   URI,
   {
-    useCreateIndex: true,
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
   (err) => {
     if (err) throw err;
-    console.log("Connected to mongodb");
+    console.log("Connected to MongoDB");
   }
 );
 
