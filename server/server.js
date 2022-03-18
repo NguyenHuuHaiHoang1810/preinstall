64bd3c77a7da1a3ab1af12fc11ab7e574b159867
NguyenHuuHaiHoang1810 <95134351+NguyenHuuHaiHoang1810@users.noreply.github.com>
2022-03-18 23:09:54 +0700

    hoangcommit

--- server/server.js
+++ server/server.js
@@ -3,16 +3,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 const URI = process.env.MONGODB_URL;
 mongoose.connect(
   URI,
   {
+    // useCreateIndex: true,
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
