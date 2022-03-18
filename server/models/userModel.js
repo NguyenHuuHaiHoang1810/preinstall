586a5639ada530a7662a4e1ca369e25a95ee0b71
NguyenHuuHaiHoang1810 <95134351+NguyenHuuHaiHoang1810@users.noreply.github.com>
2022-03-18 23:55:22 +0700

    commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,5 +14,15 @@ const userSchema=new mongoose.Schema({
         type: String,
         required: true,
     },
+    role: {
+        type: Number,
+        default: 0
+    },
+    cart: {
+        type: Array,
+        default: []
+    }
+}, {
+    timestamps: true
 })
 module.export = mongoose.model('Users',userSchema)
