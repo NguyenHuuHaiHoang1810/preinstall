af47a276dbe4a93fa15343ec1db5155b5d60c188
NguyenHuuHaiHoang1810 <95134351+NguyenHuuHaiHoang1810@users.noreply.github.com>
2022-03-19 21:15:07 +0700

    icon updated

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -44,9 +44,59 @@ const userCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await Users.findOne({ email });
+      if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Incorrect password." });
+
+      // If login success , create access token and refresh token
+      const accesstoken = createAccessToken({ id: user._id });
+      const refreshtoken = createRefreshToken({ id: user._id });
+
+      res.cookie("refreshtoken", refreshtoken, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // !7d
+      });
+
+      res.json({ accesstoken });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  logout: async (req, res) => {
+    try {
+      res.clearCookie("refreshtoken", { path: "/user/refresh_token" });
+      return res.json({ msg: "Logged out" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
   refreshToken: (req, res) => {
-    const rf_token = req.cookies.refreshtoken;
-    res.json({ rf_token });
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token)
+        return res.status(400).json({ msg: "Please Login or Register" });
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(400).json({ msg: "Please Login or Register" });
+
+        const accesstoken = createAccessToken({ id: user.id });
+
+        res.json({ accesstoken });
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
   },
 };
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 const userCtrl = require("../controllers/userCtrl");
 
 router.post("/register", userCtrl.register);
+
 router.get("/refresh_token", userCtrl.refreshToken);
+
+router.post('/login',userCtrl.login);
+
+router.get('/logout', userCtrl.logout);   //!Get hay Post đều được
 
 module.exports = router;
