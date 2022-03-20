5f4b7e3ddf91317648bf6395a1ad761d7f524257
thegr8mctin <hungnb.b19vt165@stu.ptit.edu.vn>
2022-03-20 22:54:58 +0700

    mjn

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">4H-Shop</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -11,11 +11,23 @@ export const Header = () => {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Shop Cún Cưng <span class="sr-only">(current)</span></a>
       </li>
+
+
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Shop Mèo Cưng <span class="sr-only">(current)</span></a>
+      </li>
+
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Đồ Thú Y</a>
       </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liên Hệ</a>
+      </li>
+
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
@@ -27,16 +39,20 @@ export const Header = () => {
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+      
+  
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
     </form>
+
+
   </div>
 </nav>
+
+
+
   )
 }
 export default Header
