29006850a14276e83a0877eb2a683bc2063643a2
littlepeaya <thuhuong.vt1503@gmail.com>
2022-03-22 14:00:51 +0700

    Merge branch 'main' of https://github.com/NguyenHuuHaiHoang1810/preinstall

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -15,6 +15,11 @@ cloudinary.config({
 router.post('/upload',(rep,res)=>{
     try{
         console.log(req.files)
+        if (!req.files || Object.keys(req.files).length==0)
+            return res.status(400).send("No files were uploaded.")
+
+            const file = req.files;
+            if(file.size>1024*1024) return res.status(400).json({msg: "Size too large"})
         res.json('test upload')
     }catch(err){
         res.status(500).json({msg: err.message})
