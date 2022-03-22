ee8558c160a713f213e591bdcd877ec10d59be74
thegr8mctin <hungnb.b19vt165@stu.ptit.edu.vn>
2022-03-22 17:37:08 +0700

    jsdfasd

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -23,9 +23,9 @@ export const Header = () => {
       <li class="nav-item active">
         <a class="nav-link" href="#">Đồ Thú Y <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item active">
+      {/* <li class="nav-item active">
         <a class="nav-link" href="#">Thương Hiệu <span class="sr-only">(current)</span></a>
-      </li>
+      </li> */}
       <li class="nav-item active">
         <a class="nav-link" href="#">Giới Thiệu <span class="sr-only">(current)</span></a>
       </li>
