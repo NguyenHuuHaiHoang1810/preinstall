09f6475031bcefd0aa0eec618479629090c1a3f7
littlepeaya <thuhuong.vt1503@gmail.com>
2022-03-23 00:18:18 +0700

    commit

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,6 +1,7 @@
 const router = require ('express').Router()
 const cloudinary = require ('cloudinary')
 const auth = require ('../middleware/authAdmin')
+const fs = require ('fs')
 
 //we will upload image on cloudinary
 
@@ -10,29 +11,60 @@ cloudinary.config({
     api_secret:process.env.CLOUD_API_SECRET
 })
 
-//upload image
+//upload image only by ad
 
-router.post('/upload',(rep,res)=>{
+router.post('/upload',auth,authAdmin, (rep,res)=>{
     try{
         console.log(req.files)
         if (!req.files || Object.keys(req.files).length==0)
             return res.status(400).send("No files were uploaded.")
 
             const file = req.files;
-            if(file.size>1024*1024) return res.status(400).json({msg: "Size too large"})
+            if(file.size>1024*1024) {
+                removeTmp(file.tempFilePath)
+                return res.status(400).json({msg: "Size too large"})
+            }
 
-            if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png')
-                    return res.status(400).json({msg:"File format is incorrect."})
+            if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+                removeTmp(file.tempFilePath)
+                return res.status(400).json({msg:"File format is incorrect."})
+            }
+
             cloudinary.v2.uploader.upload(file.tempFilePath,{folder:"test"},async(err,result)=>{
                 if(err) throw err;
 
-                res.json({result})
+                //after upload will have file tmp
+                removeTmp(file.tempFilePath)
+                res.json({public_id: result.public_id, url:result.secure_url})
             })
             res.json('test upload')
     }catch(err){
-        res.status(500).json({msg: err.message})
+        return res.status(500).json({msg: err.message})
     }
 
 })
 
+//delete image only by ad
+router.post ('/destroy',auth,authAdmin, (req,res)=>{
+    try{
+        const {public_id} = req.body;
+        if(!public_id) return res.status(400).json({msg:'No images selected'})
+
+        cloudinary.v2.uploader.destroy(public_id,async(err,result)=>{
+            if(err) throw err;
+
+            res.json({msg:"deleted image"})
+        })
+    } catch (err){
+        return res.status(500).json({msg:err.message})
+    }
+    
+})
+
+
+const removeTmp = (path) =>{
+    fs.unlink(path,err => {
+
+    })
+}
 module.exports = router
