ba9658714dce79ff9bd55cf8f6a93b29ad2622d4
NguyenHuuHaiHoang1810 <95134351+NguyenHuuHaiHoang1810@users.noreply.github.com>
2022-03-23 20:40:05 +0700

    route updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {DataProvider} from './GlobalState'
-import  Header  from './components/headers/Header';
-//import Products from './components/home/products/Products';
-import Filters from './components/home/products/Filters';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./GlobalState";
+import Header from "./components/headers/Header";
+import Pages from "./components/home/Pages";
+
 function App() {
   return (
     <DataProvider>
       <Router>
         <div className="App">
-          <Header/>
-          <Filters />
+          <Header />
+          <Pages />
         </div>
       </Router>
     </DataProvider>
-   
   );
 }
 
