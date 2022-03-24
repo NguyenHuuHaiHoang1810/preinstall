2ee7e3c2674467a9b62f62644c6b9f12138020ad
VuThiVanAnh123 <vuvananh1232001@gmail.com>
2022-03-24 20:49:31 +0700

    creatproduct updated

---
+++ client/src/components/home/history/OrderHistory.js
@@ -0,0 +1,32 @@
+import React, {useContext, useEffect} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
+function OderHistory() {
+    const state = useContext(GlobalState)
+    const [history, setHistory] = state.userAPI.history
+    const [isAdmin] = state.userAPI.isAdmin
+    const [token] = state.token
+
+    useEffect(() => {
+        if(token){
+          const getHistory = async() =>{
+            if(isAdmin){
+             const res = await axios.get('/api/payment', {
+               headers: {Authorization: token}
+             })
+             setHistory(res.data)
+            }else{
+             const res = await axios.get('/user/history', {
+               headers: {Authorization: token}
+             })
+             setHistory(res.data)
+            }
+          }
+          getHistory()
+        }
+      },[token, isAdmin, setHistory])
+}
+
+export default OderHistory;
