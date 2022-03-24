c19b80bcbafe68e4a676261ff4eeb2a0f7585fbc
littlepeaya <thuhuong.vt1503@gmail.com>
2022-03-24 22:50:13 +0700

    bunas noches

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -12,6 +12,19 @@ const productCtrl = {
     },
     createProduct :async (req,res)=>{
         try{
+                const {product_id, title,price,description,content, images, category} = req.body;
+                if (!images) return res.status(400).json({msg:"không có ảnh nào được upload"})
+
+                const product = await Products.findOne({product_id})
+                if (product)
+                    return res.status(400).json({msg:"sản phẩm này đã tồn tại"})
+
+                const newProduct = new Products({
+                    product_id,title:title.toLowerCase(),price,description,content,images,category
+                })
+
+                res.json(newProduct)
+
 
         }catch (err){
             return res.status(500).json({msg:err.message})
