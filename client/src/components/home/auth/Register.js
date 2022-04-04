4c56d86dc00e83feb29ba82dbed7f111f2cb671d
NguyenHuuHaiHoang1810 <95134351+NguyenHuuHaiHoang1810@users.noreply.github.com>
2022-04-04 21:30:59 +0700

    fix

--- client/src/components/home/auth/Register.js
+++ client/src/components/home/auth/Register.js
@@ -1,37 +1,41 @@
-import React, {useReducer, useState} from 'react'
-import {Link} from "react-router-dom"
-import axios from 'axios'
-function Register(){
-        const [user, setUser] = useState({
-           name:'', email: '', password: ''
-        })
-        
-        const onChangeInput = e =>{
-            const {name, value} = e.target;
-            setUser({...useReducer, [name]:value})
-        }
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+function Register() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-        const registerSubmit = async e => {
-            e.preventDefault()
-            try{
-                await axios.post('user/register', {...user})
-                localStorage.setItem('firstLogin',true)
-                window.location.href = "/";
-            } catch (err){
-                alert(err.response.data.msg)
-             }
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
 
-        }
+  const registerSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/user/register", { ...user });
+      localStorage.setItem("firstLogin", true);
+      window.location.href = "/";
+    } catch (err) {
+      alert(err.response.data.msg);
+    }
+  };
 
-
-
-
-    return (
-        <div className="login-page">
-            <form onSubmit={registerSubmit}>
-                <h2>Register</h2>
-                <input type="text" name="name" required
-                placeholder="Name" value={user.name} onChange={onChangeInput} />
+  return (
+    <div className="login-page">
+      <form onSubmit={registerSubmit}>
+        <h2>Register</h2>
+        <input
+          type="text"
+          name="name"
+          required
+          placeholder="Name"
+          value={user.name}
+          onChange={onChangeInput}
+        />
 
         <input
           type="email"
@@ -42,18 +46,21 @@ function Register(){
           onChange={onChangeInput}
         />
 
-                <input type="password" name="password" required
-                placeholder="Password" value={user.user.password} onChange={onChangeInput} />
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="Password"
+          value={user.user.password}
+          onChange={onChangeInput}
+        />
 
-            <div className="row">
-                <button type = "submit">Register</button>
-                <Link to ="/login">Login</Link>
-
-                </div>
-
-
-            </form>
+        <div className="row">
+          <button type="submit">Register</button>
+          <Link to="/login">Login</Link>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
-export default Register
+export default Register;
